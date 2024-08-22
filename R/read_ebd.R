01bf60dbef35296a6d3784bbd4ef23bd8f19000c
Karthik Thrikkadeeri <rikudoukarthik@gmail.com>
2024-08-22 19:36:17 +0530

    refined read.ebd() and added read.mydata()

--- R/read_ebd.R
+++ R/read_ebd.R
@@ -1,21 +1,30 @@
-#' EBD Input
+#' Load eBird data files
 #'
-#' Reads a .txt EBD file and creates a data frame from it, with cases corresponding to
-#' lines and variables to fields in the file. This function is a wrapper around
-#' `utils::read.delim()`, which is considerably faster than the `readr::read_delim()` used in
-#' `auk::read_ebd()`. Moreover, unlike the latter which uses snake case for column names,
-#' this function uses uppercase with period separators.
+#' @description
 #'
-#' Since EBD contains several columns, which may not all be required for a given usecase,
+#' Reads a .txt eBird data file and creates a data frame from it, with cases corresponding to
+#' lines (rows) and variables to fields (columns) in the file.
+#'
+#' The most commonly used types of eBird data files are the eBird Basic Dataset (EBD;
+#' which may contain three subtypes of files) and the My Data download
+#' (which contains all data associated with a specific eBird account). The two differ in
+#' their download file type, column naming format, available columns, etc.
+#'
+#' `read.ebd` and `read.mydata` import the EBD and My Data files respectively. Since EBD contains several columns, which may not all be required for a given usecase,
 #' `cols_sel` can be used to import only a subset of the columns. To see the list of all
-#' columns names to choose from, run `read.ebd(ebd_path, cols_print_only = TRUE`.
+#' columns names to choose from, run `read.ebd(ebd_path, cols_print_only = TRUE)`.
 #'
-#' @param ebd_path character; the path to the downloaded EBD .txt file
+#' This function is a wrapper around `utils::read.delim()`, which is considerably faster
+#' than the `readr::read_delim()` used in `auk::read_ebd()`. Moreover, unlike the latter
+#' which uses snake case for column names, this function uses uppercase with period separators.
+#'
+#' @param path character; the path to the downloaded EBD .txt file
 #' @param cols_sel character; vector of column names to be imported from the dataset
 #' @param cols_print_only logical; whether or not to only print the full set of column
 #'   names
 #'
-#' @return A data frame
+#' @return A data frame (`cols_print_only == FALSE`), or a character vector of column names
+#'  (`cols_print_only == TRUE`)
 #'
 #' @importFrom utils read.delim
 #' @export
@@ -29,9 +38,10 @@
 #'
 #' # select columns and import data
 #' read.ebd(tf, cols_sel = c("SAMPLING.EVENT.IDENTIFIER", "COMMON.NAME"))
-read.ebd <- function(ebd_path, cols_sel = "all", cols_print_only = FALSE) {
 
-  cols_all <- names(utils::read.delim(ebd_path, nrows = 1,
+read.ebd <- function(path, cols_sel = "all", cols_print_only = FALSE) {
+
+  cols_all <- names(utils::read.delim(path, nrows = 1,
                                       sep = "\t", header = TRUE, quote = "",
                                       stringsAsFactors = FALSE, na.strings = c("", " ", NA)))
 
@@ -41,24 +51,24 @@ read.ebd <- function(ebd_path, cols_sel = "all", cols_print_only = FALSE) {
   if (cols_print_only == TRUE) {
 
     print(cols_all)
-    return(cols_all)
+    return(cols_all) # also return to allow selection from vector
 
   } else {
 
     if (identical(cols_sel, "all")) {
 
-      data <- read.delim(ebd_path,
-                         sep = "\t", header = TRUE, quote = "",
-                         stringsAsFactors = FALSE, na.strings = c("", " ", NA))
+      data <- utils::read.delim(path,
+                                sep = "\t", header = TRUE, quote = "",
+                                stringsAsFactors = FALSE, na.strings = c("", " ", NA))
 
     } else {
 
       cols_all[!(cols_all %in% cols_sel)] <- "NULL"
       cols_all[cols_all %in% cols_sel] <- NA
 
-      data <- read.delim(ebd_path, colClasses = cols_all,
-                         sep = "\t", header = TRUE, quote = "",
-                         stringsAsFactors = FALSE, na.strings = c("", " ", NA))
+      data <- utils::read.delim(path, colClasses = cols_all,
+                                sep = "\t", header = TRUE, quote = "",
+                                stringsAsFactors = FALSE, na.strings = c("", " ", NA))
 
     }
 
@@ -68,5 +78,47 @@ read.ebd <- function(ebd_path, cols_sel = "all", cols_print_only = FALSE) {
 
 }
 
-# include argument where output will be list of column names from which selection can be
-# made later
+#' @rdname read.ebd
+#' @format NULL
+#' @param cols_style_ebd logical; if `TRUE` (default), change column names in My Data to uppercase
+#'    and separated by period (COLUMN.STYLE), as in `read.ebd()`
+
+read.mydata <- function(path = "MyEBirdData.csv",
+                        cols_sel = "all", cols_print_only = FALSE, cols_style_ebd = TRUE) {
+
+  cols_all <- names(utils::read.delim(pathnrows = 1,
+                                      sep = ",", header = TRUE, quote = "",
+                                      stringsAsFactors = FALSE, na.strings = c("", " ", NA)))
+
+  # if needed, first see entire list of available columns from which selection can be
+  # made later
+
+  if (cols_print_only == TRUE) {
+
+    print(cols_all)
+    return(cols_all) # also return to allow selection from vector
+
+  } else {
+
+    if (identical(cols_sel, "all")) {
+
+      data <- utils::read.delim(path,
+                                sep = ",", header = TRUE, quote = "",
+                                stringsAsFactors = FALSE, na.strings = c("", " ", NA))
+
+    } else {
+
+      cols_all[!(cols_all %in% cols_sel)] <- "NULL"
+      cols_all[cols_all %in% cols_sel] <- NA
+
+      data <- utils::read.delim(path, colClasses = cols_all,
+                                sep = ",", header = TRUE, quote = "",
+                                stringsAsFactors = FALSE, na.strings = c("", " ", NA))
+
+    }
+
+    return(data)
+
+  }
+
+}
--- tests/testthat/helpers.R
+++ tests/testthat/helpers.R
@@ -1,4 +1,4 @@
-# To remove get_param() objects from global environment when testing
+# To remove get_param() objects from global environment when testing---------------------
 
 remove_param <- function() {
 
@@ -7,5 +7,52 @@ remove_param <- function() {
                      "real_month_lab", "real_month_num", "real_year")
 
   if (all(sapply(param_objects, exists))) rm(list = param_objects, envir = .GlobalEnv)
+
+}
+
+# create dummy My Data  -------------------------------------------------------------
+
+dummy_mydata <- function(nrows = 5) {
+
+  dummy <- dplyr::tibble(
+    `Submission ID` = paste0("S", sample(10000000:99999999, 1)) %>% rep(nrows),
+    `Common Name` = sample(india_checklist$`English Name`, replace = TRUE, size = nrows),
+    # won't match
+    `Scientific Name` = sample(india_checklist$`Scientific Name`, replace = TRUE, size = nrows),
+    `Taxonomic Order` = sample(1:11000, replace = TRUE, size = nrows),
+    Count = sample(c(1:1000, "X"), replace = TRUE, size = nrows),
+    `State/Province` = sample(c("IN-KA", "IN-KL", "IN-TN", "IN-AP"), # use admin units data when ready
+                              replace = TRUE, size = nrows),
+    County = sample(c("Bengaluru Rural", "Palakkad", "Coimbatore", "Kurnool"),
+                    replace = TRUE, size = nrows),
+    `Location ID` = paste0("L", sample(100000:999999, 1)) %>% rep(nrows),
+    Location = sample(c("GKVK Campus", "Random location"),
+                      replace = TRUE, size = nrows),
+    Latitude = runif(nrows, min = 5, max = 15),
+    Longitude = runif(nrows, min = 70, max = 85),
+    Date = seq(as.Date("2023-01-01"), as.Date("2023-12-31"), by = "day") %>%
+      sample(replace = TRUE, size = nrows),
+    Time = "06:00:00",
+    Protocol = sample(c("eBird - Traveling Count", "Historical", "eBird - Stationary Count",
+                        "eBird - Casual Observation"),
+                      replace = TRUE, size = nrows),
+    `Duration (Min)` = sample(c(NA, 1:1000),
+                      replace = TRUE, size = nrows),
+    `All Obs Reported` = sample(0:1, replace = TRUE, size = nrows),
+    `Distance Traveled (km)` = sample(c(NA, 1:10),
+                                      replace = TRUE, size = nrows),
+    `Area Covered (ha)` = NA,
+    `Number of Observers` = sample(1:50, replace = TRUE, size = nrows),
+    `Breeding Code` = sample(c("A", "C", "CF", "F", "H"), replace = TRUE, size = nrows),
+    `Observation Details` = sample(c(NA, "Seen", "Heard"),
+                                   replace = TRUE, size = nrows),
+    `Checklist Comments` = sample(c(NA, "Lovely", "No birds"),
+                                  replace = TRUE, size = nrows),
+    `ML Catalog Numbers` = sample(c(NA, paste0("ML", sample(10000000:99999999, 1))),
+                                  replace = TRUE, size = nrows),
+
+  )
+
+  return(dummy)
 
 }
