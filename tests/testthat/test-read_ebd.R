dec640a8db241d23ac51f66c9ff0477af778fca0
Karthik Thrikkadeeri <rikudoukarthik@gmail.com>
2024-09-05 10:50:45 +0530

    tests for cols_sel and cols_style_ebd

--- tests/testthat/test-read_ebd.R
+++ tests/testthat/test-read_ebd.R
@@ -1,6 +1,6 @@
 test_that("cols_print_only works (prints and returns column names)", {
 
-  expect_equal(
+  expect_contains(
 
     read.ebd(test_path("test-ebd.txt"), cols_print_only = TRUE),
     c("LAST.EDITED.DATE","CATEGORY","COMMON.NAME","EXOTIC.CODE","OBSERVATION.COUNT",
@@ -8,11 +8,70 @@ test_that("cols_print_only works (prints and returns column names)", {
       "LOCALITY.TYPE","LATITUDE","LONGITUDE","OBSERVATION.DATE","TIME.OBSERVATIONS.STARTED",
       "OBSERVER.ID","SAMPLING.EVENT.IDENTIFIER","PROTOCOL.TYPE","DURATION.MINUTES",
       "EFFORT.DISTANCE.KM","NUMBER.OBSERVERS","ALL.SPECIES.REPORTED","GROUP.IDENTIFIER",
-      "HAS.MEDIA","APPROVED","REVIEWED","TRIP.COMMENTS","SPECIES.COMMENTS","GROUP.ID",
-      "YEAR","MONTH","DAY.M","M.YEAR","M.MONTH")
+      "HAS.MEDIA","APPROVED","REVIEWED","TRIP.COMMENTS","SPECIES.COMMENTS")
 
   )
 
   expect_output(read.ebd(test_path("test-ebd.txt"), cols_print_only = TRUE))
 
 })
+
+test_that("cols_style_ebd works", {
+
+  expect_contains(
+    read.mydata(test_path("MyEBirdData.csv"), cols_print_only = TRUE, cols_style_ebd = TRUE),
+    c("SAMPLING.EVENT.IDENTIFIER", "COMMON.NAME")
+  )
+  expect_contains(
+    read.mydata(test_path("MyEBirdData.csv"), cols_print_only = TRUE, cols_style_ebd = FALSE),
+    c("Submission ID", "Common Name")
+  )
+
+  expect_failure(
+    expect_contains(
+      read.mydata(test_path("MyEBirdData.csv"), cols_print_only = TRUE, cols_style_ebd = TRUE),
+      c("Submission ID", "Common Name")
+    )
+  )
+
+  expect_failure(
+    expect_contains(
+      read.mydata(test_path("MyEBirdData.csv"), cols_print_only = TRUE, cols_style_ebd = FALSE),
+      c("SAMPLING.EVENT.IDENTIFIER", "COMMON.NAME")
+    )
+  )
+
+})
+
+test_that("cols_sel works", {
+
+  col_to_sel <- "COMMON.NAME"
+  expect_equal(
+    names(read.ebd(test_path("test-ebd.txt"), cols_print_only = FALSE,
+                   cols_sel = col_to_sel)),
+    col_to_sel
+  )
+
+  col_to_sel <- "COMMON.NAME"
+  expect_equal(
+    names(read.mydata(test_path("MyEBirdData.csv"), cols_print_only = FALSE, cols_style_ebd = TRUE,
+                      cols_sel = col_to_sel)),
+    col_to_sel
+  )
+
+    col_to_sel <- "Common Name"
+  expect_equal(
+    names(read.mydata(test_path("MyEBirdData.csv"), cols_print_only = FALSE, cols_style_ebd = FALSE,
+                      cols_sel = col_to_sel)),
+    col_to_sel
+  )
+
+})
+
+# c("LAST.EDITED.DATE","CATEGORY","COMMON.NAME","EXOTIC.CODE","OBSERVATION.COUNT",
+#   "BREEDING.CODE","STATE","STATE.CODE","COUNTY","COUNTY.CODE","LOCALITY","LOCALITY.ID",
+#   "LOCALITY.TYPE","LATITUDE","LONGITUDE","OBSERVATION.DATE","TIME.OBSERVATIONS.STARTED",
+#   "OBSERVER.ID","SAMPLING.EVENT.IDENTIFIER","PROTOCOL.TYPE","DURATION.MINUTES",
+#   "EFFORT.DISTANCE.KM","NUMBER.OBSERVERS","ALL.SPECIES.REPORTED","GROUP.IDENTIFIER",
+#   "HAS.MEDIA","APPROVED","REVIEWED","TRIP.COMMENTS","SPECIES.COMMENTS","GROUP.ID",
+#   "YEAR","MONTH","DAY.M","M.YEAR","M.MONTH")
